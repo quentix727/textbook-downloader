3df78c43abc1a63b9b300a7137821b4a0f0a59fd
quentix727 <quentix727@outlook.com>
2024-02-22 11:06:00 +0800

    update
    目前的问题：无法删除最后一张图片和文件夹；只适用于windows10用户，且python3.8及以下版本会报错。

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 # -*- coding:utf-8 -*-
+'''
+This program is only for Windows system users.
+'''
 import os
 import time as t
-# import shutil
 import random as r
 import wget
 from PIL import Image
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
-bk = input('book number:')
-pgnum = int(input('page number:'))
-string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#^()-_'
-path = ''
-start_t = t.time()
-c = 'cls'
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+TEMP_FOLDER = os.path.expanduser("~") + '\\AppData\\Local\\Temp\\'
 
-for i in range(r.randint(10,100)):
-    path = path + string[r.randint(0, 58)]
+def clear_screen():
+    os.system('cls')
 
-tmp = os.path.expanduser("~") + '\\AppData\\Local\\Temp\\' + path
-os.mkdir(tmp)
+def download_images(bk, pgnum, tmp):
+    clear_screen()
+    start_t = t.time()
+    
+    for i in range(1, pgnum + 1):
+        num = str(i).zfill(3)
+        percent = int(i / pgnum * 100)
 
-os.system(c)
-for i in range(1, pgnum + 1):
-    num = str(i).zfill(3)
-    percent = int(i / pgnum * 100)
+        wget.download('https://book.pep.com.cn/' + bk + '/files/mobile/' + str(i) + '.jpg', tmp + '\\' + bk + '_' + num + '.jpg')
+        clear_screen()
+        print(str(percent) + '% | ' + '#' * percent)
 
-    wget.download('https://book.pep.com.cn/' + bk + '/files/mobile/' + str(i) + '.jpg', tmp + '\\' + bk + '_' + num + '.jpg')
-    os.system(c)
-    print(str(percent) + '% | ' + '#' * percent)
-
-os.system(c)
-end_t = t.time()
-print('Time costs ' + str(end_t - start_t) + 's.')
-t.sleep(0.5)
-print('Please wait for a moment while we are merge the images to PDF...')
+    clear_screen()
+    end_t = t.time()
+    print('Time costs ' + str(end_t - start_t) + 's.')
+    t.sleep(0.5)
+    print('Please wait for a moment while we are merging the images to PDF...')
 
 def images_to_pdf(image_folder, output_pdf):
     c = canvas.Canvas(output_pdf, pagesize=letter)
@@ -47,7 +45,24 @@ def images_to_pdf(image_folder, output_pdf):
 
     c.save()
     print(f"PDF file saved as : {output_pdf}")
-    # 这里要删除临时文件
 
-images_to_pdf(tmp, bk + ".pdf")
+    for filename in os.listdir(image_folder):
+        file_path = os.path.join(image_folder, filename)
+        os.remove(file_path)
+    os.rmdir(image_folder)
 
+if __name__ == "__main__":
+    bk = input('book number:')
+    pgnum = int(input('page number:'))
+    
+    path = ''
+    for i in range(r.randint(10, 100)):
+        path = path + ALPHABET[r.randint(0, len(ALPHABET) - 1)]
+
+    tmp = os.path.join(TEMP_FOLDER, path)
+
+    if not os.path.exists(tmp):
+        os.mkdir(tmp)
+
+    download_images(bk, pgnum, tmp)
+    images_to_pdf(tmp, bk + ".pdf")
